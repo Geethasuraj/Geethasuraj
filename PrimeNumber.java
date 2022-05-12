24d758e9a264a26f3ca37a91c52be117e078df0c
Geethasuraj <88611068+Geethasuraj@users.noreply.github.com>
2022-05-12 15:02:42 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -4,11 +4,14 @@ public class PrimeNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 24;
+		int a = 26;
 		boolean flag = false;
 		for (int i = 1; i < 8; i++) {
 			if ( a % i == 0) {
+				if(a%2==0) 
+				{
 				flag= true;
+				}
 				break;
 			}
 		}
